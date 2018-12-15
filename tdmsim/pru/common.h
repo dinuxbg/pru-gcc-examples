597640a118df4c963350f3503c32a894d4e8a306
Dimitar Dimitrov <dimitar@dinux.eu>
2018-12-15 21:37:37 +0200

    Add tdmsim project
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

---
+++ tdmsim/pru/common.h
@@ -0,0 +1,27 @@
+/* common.h - Common constant definitions
+ *
+ * Copyright (c) 2018 Dimitar Dimitrov <dimitar@dinux.eu>
+ *
+ * The authors hereby grant permission to use, copy, modify, distribute,
+ * and license this software and its documentation for any purpose, provided
+ * that existing copyright notices are retained in all copies and that this
+ * notice is included verbatim in any distributions. No written agreement,
+ * license, or royalty fee is required for any of the authorized uses.
+ * Modifications to this software may be copyrighted by their authors
+ * and need not follow the licensing terms described here, provided that
+ * the new terms are clearly indicated on the first page of each file where
+ * they apply.
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define SCRATCH_BANK_0	10
+#define SCRATCH_BANK_1	11
+#define SCRATCH_BANK_2	12
+#define PEER_PRU_BANK	14
+
+#define TDM_SLOT_SIZE	32
+#define TDM_NUM_SLOTS	2
+
+#endif	/* __COMMON_H__ */
