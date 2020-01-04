6f3c17a28c48925feac2aeb8f75245c3226d4f6c
Dimitar Dimitrov <dimitar@dinux.eu>
2020-01-04 21:38:57 +0200

    beaglemic: Validate LED strip on PocketBeagle
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- beaglemic/libs/vis.c
+++ beaglemic/libs/vis.c
@@ -18,9 +18,22 @@ int main(int argc, char *argv[])
 	/* Warning: show-pins.pl reports gpiochip number with one
 	 * higher than what libgpiod expects!. */
 	ctx = shift_init(argv[0],
+
+#if 0
+			/* BB AI */
 			"gpiochip4", 12,  /* DS:   P9.30 */
 			"gpiochip3", 17,  /* SHCP: P9.28 */
 			"gpiochip6", 11); /* STCP: P9.23 */
+#else
+			/* PocketBeagle
+			 *   config-pin P2_25 gpio
+			 *   config-pin P2_29 gpio
+			 *   config-pin P2_31 gpio
+			 */
+			"gpiochip1", 9,   /* DS:   P2.25 , GPIO41 */
+			"gpiochip0", 7,   /* SHCP: P2.29 , GPIO7 */
+			"gpiochip0", 19); /* STCP: P2.31 , GPIO19 */
+#endif
 
 
 	for (i = 0; i < 16; i++) {
