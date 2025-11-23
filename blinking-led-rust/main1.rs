0a4ec2bb701abf731a91a48e740996ceb91d955f
Dimitar Dimitrov <dimitar@dinux.eu>
2025-11-23 21:55:59 +0200

    rust: Add I/O operations
    gccrs is still highly experimental, but it can now do I/O!
    
    Tested with the non-released r16-5244-g5a2319b71e4d30 GCC version.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- blinking-led-rust/main1.rs
+++ blinking-led-rust/main1.rs
@@ -1,3 +1,24 @@
+#![feature(rustc_attrs)]
+
+#[rustc_builtin_macro]
+macro_rules! asm {
+    () => {}
+}
+
 fn main() {
+    let ones: u32 = 0xffff;
+    let zeros: u32 = 0;
+    unsafe {
+        asm!(
+            "mov r30, {}",
+            "nop",
+            in(reg) ones,
+        );
+        asm!(
+            "mov r30, {}",
+            "nop",
+            in(reg) zeros,
+        );
+    }
     loop {}
 }
