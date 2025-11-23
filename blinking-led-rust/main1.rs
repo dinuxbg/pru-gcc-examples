3fcc928c9c3b2b08d480ebf55d988d13cdf2c9ed
Dimitar Dimitrov <dimitar@dinux.eu>
2025-11-23 22:20:23 +0200

    rust: Add I/O delays
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- blinking-led-rust/main1.rs
+++ blinking-led-rust/main1.rs
@@ -5,20 +5,28 @@ macro_rules! asm {
     () => {}
 }
 
+extern "C" {
+    fn delay_us(input: u32) -> ();
+}
+
 fn main() {
     let ones: u32 = 0xffff;
     let zeros: u32 = 0;
-    unsafe {
-        asm!(
-            "mov r30, {}",
-            "nop",
-            in(reg) ones,
-        );
-        asm!(
-            "mov r30, {}",
-            "nop",
-            in(reg) zeros,
-        );
+    let period_us: u32 = 250 * 1000;
+    loop {
+        unsafe {
+            asm!(
+                "mov r30, {}",
+                "nop",
+                in(reg) ones,
+            );
+            delay_us(period_us);
+            asm!(
+                "mov r30, {}",
+                "nop",
+                in(reg) zeros,
+            );
+            delay_us(period_us);
+        }
     }
-    loop {}
 }
